917cb0cb0c48c045e91e1a96a5fd60a8a82755f3
baksohyeon <bori12370@gmail.com>
2022-11-10 16:26:16 +0900

    feat(baseball): create skeleton code

---
+++ src/main/java/baseball/domain/Judgement.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+public class Judgement {
+    public int correctCount() {
+    return null;
+    }
+
+    public boolean hasReplace(int index, int number) {
+        return false;
+    }
+
+}
---
+++ src/main/java/baseball/domain/Referee.java
@@ -0,0 +1,9 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Referee {
+    public String void compare(List<Integer> computer, List<Integer> player) {
+
+    }
+}
